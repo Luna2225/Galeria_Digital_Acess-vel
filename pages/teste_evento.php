19e2c768d64c62e2fee386d8fc70fcb1899b2ecc
Daiane3006 <daiane.correia.oliveira77@gmail.com>
2023-05-26 18:18:08 -0300

    descrição do evento com botão de cadastrar obra

---
+++ pages/teste_evento.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en" prefix="og: https://ogp.me/ns#">
+
+<head>
+    <meta property="og:title" content="The Rock" />
+    <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+    <meta property="og:description" content="Sean Connery found fame and fortune as the
+           suave, sophisticated British agent, James Bond." />
+    <meta property="og:type" content="article" />
+    <meta property="whatsapp/site" content="https://br.freepik.com/fotos/flores" />
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <script src="https://kit.fontawesome.com/54ae61cac0.js" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="../assets/css/teste-evento.css">
+
+    <title>Descrição do Evento | Arceble</title>
+</head>
+
+<body>
+
+    <nav class="navbar" id="menu">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="eventos.html" id="logo"><i class="fa-solid fa-arrow-left"></i> ARCEBLE</a>
+        </div>
+    </nav>
+
+    <center>
+        <div class="container-fluid" id="container">
+
+            <!-- === MIGALHAS DE PÃO === -->
+            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);">
+                <ul class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="eventos.html">Eventos</a></li>
+                    <li class="breadcrumb-item acitve">Descrição do Evento</li>
+                </ul>
+            </nav>
+
+            <!-- <div class="img_even"></div> -->
+            <img src="../assets/img/britt-gaiser-hSAlu33padA-unsplash.jpg" class="img-fluid img_even" alt="..."><br>
+            <audio controls autoplay>
+                <source src="assets/audio/Audiodescrição obra A Noite Estrelada de Vincent Van Gogh.mp3" type="audio/mpeg">
+            </audio>
+
+            <h1>Nome da Obra
+                <!-- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
+                        class="bi bi-share-fill" viewBox="0 0 16 16">
+                        <path
+                            d="M11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5z" />
+                    </svg>
+                </button> -->
+            </h1>
+
+            <!-- Modal -->
+            <!-- <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Compartilhar</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <a href="whatsapp://send?text=Nome da obra:%0ahttps://br.freepik.com/fotos/flores"
+                                style="color: darkgreen;"><i
+                                    class="fa-brands fa-whatsapp fa-3x"></i><br>whatsapp</a><br>
+                            <a href="
+                            https://www.facebook.com/sharer/sharer.php?u=[URL]" style="color: blue;"><i
+                                    class="fa-brands fa-facebook fa-3x"></i><br>Facebook</a><br>
+                            <a href="" style="color: rgb(129, 39, 151);"><i
+                                    class="fa-brands fa-instagram fa-3x"></i><br>Instagram</a>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
+
+            <p>Descrição</p>
+            <hr>
+
+            <h2>Obras Cadastradas
+                <button type="button" class="btn btn-primary botao" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                    Cadastrar
+                </button>
+            </h2>
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Cadastrar obra</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row">
+                                <div class="col">
+                                    <img src="../assets/img/01.jpg" alt="">
+                                    <h6>nome da obra</h6>
+                                    <p>nome do autor</p>
+
+                                    <label>
+                                        <input type="checkbox" class="input">
+                                        <i class="btn btn-primary bt">Selecionar</i>
+                                    </label>
+                                </div>
+
+                                <div class="col">
+                                    <img src="../assets/img/01.jpg" alt="">
+                                    <h6>nome da obra</h6>
+                                    <p>nome do autor</p>
+
+                                    <label>
+                                        <input type="checkbox" class="input">
+                                        <i class="btn btn-primary bt">Selecionar</i>
+                                    </label>
+                                </div>
+
+                                <div class="col">
+                                    <img src="../assets/img/01.jpg" alt="">
+                                    <h6>nome da obra</h6>
+                                    <p>nome do autor</p>
+
+                                    <label>
+                                        <input type="checkbox" class="input">
+                                        <i class="btn btn-primary bt">Selecionar</i>
+                                    </label>
+                                </div>
+
+                                <div class="col">
+                                    <img src="../assets/img/01.jpg" alt="">
+                                    <h6>nome da obra</h6>
+                                    <p>nome do autor</p>
+
+                                    <label>
+                                        <input type="checkbox" class="input">
+                                        <i class="btn btn-primary bt">Selecionar</i>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-primary">Salvar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container-fluid im">
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card img">
+                            <img src="../assets/img/01.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional
+                                    content.</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <a href="#" class="btn btn-primary ">Ver</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card img">
+                            <img src="../assets/img/01.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional
+                                    content.</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <a href="#" class="btn btn-primary ">Ver</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card img">
+                            <img src="../assets/img/01.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional
+                                    content.</p>
+                                <div class="d-grid gap-2 col-6 mx-auto">
+                                    <a href="#" class="btn btn-primary ">Ver</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </center>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+
+    <script src="assets/js/modal.js"></script>
+
+    <script>
+        var botoes = document.getElementsByClassName("bt");
+
+        for (var i = 0; i < botoes.length; i++) {
+            var botao = botoes[i];
+            botao.addEventListener("click", function() {
+                if (this.textContent === "Selecionar") {
+                    this.textContent = "Selecionado";
+                } else {
+                    this.textContent = "Selecionar";
+                }
+            });
+        }
+    </script>
+</body>
+
+</html>
